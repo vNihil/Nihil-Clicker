fccbf6259c6d7b924a3fc9b734149b9b0366c52f
vNihil <lemuspablo@protonmail.com>
2022-10-11 23:11:10 -0600

    Version 1.0

---
+++ Autoclick/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Autoclick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Click = new System.Windows.Forms.Timer(this.components);
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(247, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Click Derecho";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(135, 26);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Click Izquierdo";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Click
+            // 
+            this.Click.Tick += new System.EventHandler(this.Click_Tick);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(28, 26);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(101, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Ocultar al iniciar";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(416, 91);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NihilClicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer Click;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ Autoclick/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autoclick
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+
+
+        public Boolean cDerecho,cIzquierdo, ocultar;
+        public Process[] pr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                cDerecho = true;
+            }
+            if (checkBox2.Checked)
+            {
+                cIzquierdo = true;
+            }
+            if (checkBox3.Checked)
+            {
+                ocultar = true;
+            }
+            if(checkBox1.Checked && checkBox2.Checked)
+            {
+                cIzquierdo = true;
+                cDerecho = true;
+            }
+            pr = Process.GetProcessesByName("javaw");
+            if (pr.Length > 0)
+            {
+                Click.Start();
+            }
+            else
+            {
+                MessageBox.Show("Debes abrir minecraft para inyectar el autoclicker");
+                return;
+            }
+
+            if (ocultar)
+            {
+                this.Hide();
+            }
+
+        }
+
+        private void Click_Tick(object sender, EventArgs e)
+        {
+            if (GetParent(pr[0]).ToString().Equals("System.Diagnostics.Process (MinecraftLauncher)"))
+            {
+                if (GetForegroundWindow() == FindWindow(null, pr[0].MainWindowTitle))
+                {
+                    if (MouseButtons == MouseButtons.Left && cIzquierdo)
+                    {
+                        SendMessage(GetForegroundWindow(), 0x201, 0, 0);
+                        Task.Delay(20).Wait();
+                        SendMessage(GetForegroundWindow(), 0x202, 0, 0);
+                    }
+                    if (MouseButtons == MouseButtons.Right && cDerecho)
+                    {
+                        SendMessage(GetForegroundWindow(), 0x0204, 0, 0);
+                        Task.Delay(20).Wait();
+                        SendMessage(GetForegroundWindow(), 0x0204, 0, 0);
+                    }
+                }
+            }
+            else if (ocultar)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Minecraft cerrado");
+                Application.Exit();
+            }
+        }
+
+        public static Process GetParent(Process process)
+        {
+            try
+            {
+                using (var query = new ManagementObjectSearcher(
+                  "SELECT * " +
+                  "FROM Win32_Process " +
+                  "WHERE ProcessId=" + process.Id))
+                {
+                    return query
+                      .Get()
+                      .OfType<ManagementObject>()
+                      .Select(p => Process.GetProcessById((int)(uint)p["ParentProcessId"]))
+                      .FirstOrDefault();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
